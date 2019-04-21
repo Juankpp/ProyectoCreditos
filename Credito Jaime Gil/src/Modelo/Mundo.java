05f543cf7910dd7a6189a227d3e6955bbd9a42ae
Maria Eugenia <Maria Eugenia@DESKTOP-VEQJC2F>
2019-04-21 14:45:35 -0500

    Version inicial programa credito Jaime Gil

---
+++ Credito Jaime Gil/src/Modelo/Mundo.java
@@ -0,0 +1,22 @@
+package Modelo;
+
+public class Mundo {
+	
+	private Credito c;
+	
+	public Mundo() {
+		c= new Credito();
+		
+	}
+
+	public Credito getC() {
+		return c;
+	}
+
+	public void setC(Credito c) {
+		this.c = c;
+	}
+	
+	
+
+}
